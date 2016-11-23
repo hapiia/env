a2781859ad37b054ceef9bad16b6de78aa717c7e
Hapii <hapii@mail.baunter.com>
2016-11-23 23:06:28 +0900

    Q@ ~

---
+++ gentoo01/copy.sh
@@ -0,0 +1,11 @@
+cd .
+cp -rf --parents /var/lib/portage/world .
+cp -rf --parents /etc/portage/make.conf .
+cp -rf --parents /etc/portage/repos.conf/local.conf .
+cp -rf --parents /etc/portage/package.use .
+cp -rf --parents /etc/portage/package.keywords .
+cp -rf --parents /etc/portage/package.mask .
+
+cp -rf --parents /usr/src/linux/.config .
+
+cp -rf --parents /usr/local/portage/* .
