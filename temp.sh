82e973149825078f1dbd28c1a45db30754eb454b
Hapii <hapii@mail.baunter.com>
2016-12-02 19:41:14 +0900

    A

---
+++ temp.sh
@@ -0,0 +1,2 @@
+mount -t cifs //192.168.179.4/share/ /mnt/share/ -o username=tatsuyasen -o uid=tatsuyasen
+mount -t cifs //192.168.179.4/vmware/ /mnt/vmware/ -o username=tatsuyasen -o uid=tatsuyasen
