8775e3ab1194791162efa9d5ab658d9ffd73d731
Hapii <hapii@mail.baunter.com>
2016-12-11 22:15:46 +0900

    A

--- gentoo01/copy.sh
+++ gentoo01/copy.sh
@@ -3,8 +3,8 @@ cp -rf --parents /var/lib/portage/world .
 cp -rf --parents /etc/portage/make.conf .
 cp -rf --parents /etc/portage/repos.conf/local.conf .
 cp -rf --parents /etc/portage/package.use .
-cp -rf --parents /etc/portage/package.keywords/ .
-cp -rf --parents /etc/portage/package.mask/ .
+cp -rf --parents /etc/portage/package.keywords .
+cp -rf --parents /etc/portage/package.mask .
 
 cp -rf --parents /etc/portage/package.license .
 
